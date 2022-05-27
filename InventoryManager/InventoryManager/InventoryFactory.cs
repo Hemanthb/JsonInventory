525636a50e2d26fae4d0e43086771090daa7f17f
hem_b <hemanthb93@gmail.com>
2022-05-27 16:18:07 +0530

    [Hemanth]:Each grain has varieties which are enlisted

---
+++ InventoryManager/InventoryManager/InventoryFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManager
+{
+    class InventoryFactory
+    {
+        public List<InventoryItems> wheat { get; set; }
+        public List<InventoryItems> rice { get; set; }
+        public List<InventoryItems> pulses { get; set; }
+    }
+}
--- InventoryManager/InventoryManager/InventoryManagement.cs
+++ InventoryManager/InventoryManager/InventoryManagement.cs
@@ -9,22 +9,53 @@ namespace InventoryManager
 {
     internal class InventoryManagement
     {
-        List<InventoryItems> inventoryItems = new List<InventoryItems>();
-        InventoryItems inventoryItem = new InventoryItems();
-
+       
+        InventoryFactory inventoryFactory = new InventoryFactory();
+        List<InventoryItems> riceVarieties = new List<InventoryItems>();
+        List<InventoryItems> wheatVarieties = new List<InventoryItems>();
+        List<InventoryItems> pulseVarieties= new List<InventoryItems>();
         public void ConvertJson(string jsonFilePath)
         {
             StreamReader reader = new StreamReader(jsonFilePath);
             {
                 var json = reader.ReadToEnd();
-                var items = JsonConvert.DeserializeObject<List<InventoryItems>>(json);
-                Console.WriteLine("   Grain       Price      Weight     TotalValue");
+                var items = JsonConvert.DeserializeObject<InventoryFactory>(json);
+                riceVarieties = items.rice;
+                wheatVarieties = items.wheat;
+                pulseVarieties = items.pulses;
+                Console.WriteLine("Enter your choice to print details of grains\n" +
+                    "1 -> Rice\n2 -> Wheat\n3 -> Pulses");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("   Grain       Price      Weight     TotalPrice");
                 Console.WriteLine("-----------------------------------------------");
-                foreach (var item in items)
+                switch (choice)
                 {
-                    double value = item.price * item.weight;
-                    Console.WriteLine("   "+item.Name+"    Rs."+item.price+"    "+item.weight+"kg    Rs."+value);
+                    case 1:
+                        foreach (var rice in riceVarieties)
+                        {
+                            double value = rice.price * rice.weight;
+                            Console.WriteLine("   " + rice.Name + "    Rs." + rice.price + "    " + rice.weight + "kg    Rs." + value);
+                        }
+                        break;
+                    case 2:
+                        foreach (var wheat in wheatVarieties)
+                        {
+                            double value = wheat.price * wheat.weight;
+                            Console.WriteLine("   " + wheat.Name + "    Rs." + wheat.price + "    " + wheat.weight + "kg    Rs." + value);
+                        }
+                        break;
+                    case 3:
+                        foreach (var pulses in pulseVarieties)
+                        {
+                            double value = pulses.price * pulses.weight;
+                            Console.WriteLine("   " + pulses.Name + "    Rs." + pulses.price + "    " + pulses.weight + "kg    Rs." + value);
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Enter a valid input from 1-3");
+                        break;
                 }
+                
             }
         }
     }
